984f795e0291a480f2bbde23a47b8ece55c1fc21
Rishabh Mishra <152280270+rishu449@users.noreply.github.com>
2025-03-30 17:57:05 +0000

    variable config

---
+++ variable.js
@@ -0,0 +1,8 @@
+const stdId=449
+let password='1234'
+var emailId="abc@gmail.com"
+var emailId="abd@gmail.com"
+console.table([stdId,emailId,password])
+//const used for constant values/data
+//let used for changeable data/value
+//var is not prefered to be used
